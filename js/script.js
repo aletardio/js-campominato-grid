97716447e5bb1c0eca6ce2705d6adb8b057e3292
aletardio <ale.tardio99@gmail.com>
2023-11-29 18:43:39 +0100

    Added functions for Super Bonus

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
-// Creazione del pulsante che genera la griglia di gioco
-const button = document.querySelector('#generate-button');
-// Variabile di stato per verificare se la griglia è già stata generata
-let gridGenerated = false; 
-button.addEventListener('click', function() {
-    // Se la griglia è già stata generata, esce dalla funzione
-    if(gridGenerated) {
-        return;
-    }
-    gridGenerated = true;
+function createPlayGround(totalCells, cellsPerSide) {
 
-    // Ottiene il riferimento al contenitore della griglia
-    const grid = document.getElementById('grid');
-    
-    // Definire un ciclo for per la realizzazione delle celle da 1 a 100
-    for (let i=0; i<100; i++) {
-        // Creazione della singola cella
-        let square = createCell(i+1);
-        // Al click la cella cambierà di colore e la console emette il numero della cella
-        square.addEventListener('click', function(){
-            this.classList.toggle('clicked');
-            console.log(square);
-        })
-        // Appendo la cella alla griglia
-        grid.appendChild(square);
-    }
-})
+        // Ottiene il riferimento al contenitore della griglia
+        const grid = document.getElementById('grid');
+        // Definire un ciclo for per la realizzazione delle celle da 1 a 100
+        for (let i=0; i<totalCells; i++) {
+            // Creazione della singola cella
+            let square = createCell(i+1, cellsPerSide);
+            // Al click la cella cambierà di colore e la console emette il numero della cella
+            square.addEventListener('click', function(){
+                this.classList.toggle('clicked');
+                console.log(square);
+            })
+            // Appendo la cella alla griglia
+            grid.appendChild(square);
+        }
+}
+
 
 // Funzione che permette di creare la singola casella della griglia
-function createCell (num) {
+function createCell (num, numPerSide) {
 
     
     const element = document.createElement('div');
     element.classList.add('square');
+    
+    let sideLenght = `calc(50vw / ${numPerSide})`;
+    const grid = document.getElementById('grid');
+    grid.style.width = numPerSide * sideLenght;
+    grid.style.height = numPerSide * sideLenght;
+
+
+    element.style.width = sideLenght;
+    element.style.height = sideLenght;
 
     const span = document.createElement('span');
     span.innerText = num; 
@@ -43,4 +43,54 @@ function createCell (num) {
 
 }
 
+// Funzione che mi genera la griglia in base al livello di difficoltà selezionato
+function createNewGame() {
+    document.getElementById('grid').innerHTML = '';
+
+    let levelSelector = document.getElementById('difficulty');
+    let level = parseInt(levelSelector.value);
+
+    let cellsNumber; 
+    let cellsPerSide;
+
+    // Determinazione delle caselle per livello
+    switch(level) {
+        case 1: 
+            cellsNumber = 100; 
+            cellsPerSide = 10; 
+            break;
+
+        case 2: 
+        cellsNumber = 81; 
+        cellsPerSide = 9; 
+            break;
+
+        case 3: 
+        cellsNumber = 49; 
+        cellsPerSide = 7; 
+            break;
+        default: 
+            cellsNumber = 100; 
+            cellsPerSide = 10;
+
+    }
+
+    createPlayGround(cellsNumber, cellsPerSide);
+}
+
+// // Creazione del pulsante che genera la griglia di gioco
+// const button = document.querySelector('#generate-button');
+// // Variabile di stato per verificare se la griglia è già stata generata
+// let gridGenerated = false; 
+// button.addEventListener('click', function() {
+//     // Se la griglia è già stata generata, esce dalla funzione
+//     if(gridGenerated) {
+//         return;
+//     }
+//     gridGenerated = true;
+// })
+
+document.getElementById('generate-button').addEventListener('click', function(){
+    createNewGame();
+})
 
